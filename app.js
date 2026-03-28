657a0d1f629396b1d0e822b0dbe78afd9035e3b3
paul axel <siehpaul2020@gmail.com>
2026-03-28 15:09:12 +0000

    Add JavaScript webserver app

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
-const http=require('http'); // import http from 'http'
-const server=http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello, World!');
-})
-const port = process.env.PORT || 8080;
-server.listen(port, ()=>{
-    console.log(`Server is listening on port ${port}`);
-});
+const http=require('http');
+const fs=require('fs').promises;
+const host="0.0.0.0"; // use any interface
+const port=8000;
+
+let indexFile;
+// Request listener
+const requestListener = (req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+    res.writeHead(200);
+    res.end(indexFile);
+
+};
+
+const server= http.createServer(requestListener);
+
+fs.readFile(__dirname + "/index.html")
+    .then(contents => {
+        indexFile = contents;
+        server.listen(port, host, () => {
+            console.log(`Server running at http://${host}:${port}\n  ${new Date()}`);
+        });
+    })
+    .catch(err=>{
+        console.error(`Could not read index.html file: ${err}`);
+        process.exit(1);
+    });
